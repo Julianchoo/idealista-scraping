50188dfb826cfb0f450148d7d8262a0f9be75697
Julian Korn <juliankorn@gmail.com>
2024-10-14 13:37:54 +0200

    Minor code improvements

--- idealista_final.py
+++ idealista_final.py
@@ -1,7 +1,5 @@
 # SCRAPING de IDEALISTA
-# Imports
 from selenium import webdriver
-from selenium.webdriver.firefox.service import Service
 from bs4 import BeautifulSoup
 import pandas as pd
 import time
@@ -10,6 +8,10 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from fake_useragent import UserAgent
+
+# Utility function to get element text safely
+def safe_get_text(element, class_name):
+    return element.find('span', class_=class_name).get_text() if element.find('span', class_=class_name) else None
 
 # Generate a random user agent using fake_useragent
 ua = UserAgent()
@@ -22,9 +24,6 @@ options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
 options.add_argument('--disable-blink-features=AutomationControlled')
 
-# How long to sleep
-sleep_time = 3
-
 # Run in normal (non-headless) mode to avoid detection
 driver = uc.Chrome(options=options)
 
@@ -32,89 +31,73 @@ driver = uc.Chrome(options=options)
 properties = []
 
 try:
-    # Loop through the first 2 pages
+    # Loop through the first 31 pages
     for page in range(1, 32):
-        # Update the URL to include the page number
-        if page == 1:
-            url = 'https://www.idealista.com/en/venta-viviendas/barcelona/eixample/la-dreta-de-l-eixample/'
-        else:
-            url = f'https://www.idealista.com/en/venta-viviendas/barcelona/eixample/la-dreta-de-l-eixample/pagina-{page}.htm'
+        url = f'https://www.idealista.com/en/venta-viviendas/barcelona/eixample/la-dreta-de-l-eixample/pagina-{page}.htm' if page > 1 else 'https://www.idealista.com/en/venta-viviendas/barcelona/eixample/la-dreta-de-l-eixample/'
+        
         try:
-            # Load the URL
             driver.get(url)
-            print('\n\n\n\n\n')
-            print(f'Page {page} loaded successfully')
-            time.sleep(sleep_time)  # Wait for loading
+            print(f'\nPage {page} loaded successfully')
+            time.sleep(3)  # Wait for loading
 
-            # Scroll to the bottom of the page to ensure all content is loaded
+            # Scroll to the bottom to ensure all content is loaded
             driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-            time.sleep(sleep_time)  # Additional wait after scrolling
+            time.sleep(3)
 
-            # Wait for a known element to be present before proceeding
-            # WebDriverWait(driver, sleep_time).until(
-            #     EC.presence_of_element_located((By.CSS_SELECTOR, 'div.PostingCardLayout-sc-i1odl-0'))
-            # )
-            # print('Content found on the page')
+            # Wait until property info is loaded
+            WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'div.item-info-container')))
 
-            # Get the page source
+            # Get the page source and parse it
             html = driver.page_source
             soup = BeautifulSoup(html, 'html.parser')
-            #print(soup.prettify())
 
-            #Scraping logic
-            n=1
-            for i in soup.find_all('div',class_="item-info-container"):
+            n = 1
+            for i in soup.find_all('div', class_="item-info-container"):
                 print(f"Prop {n}, page {page}")
-                #print(i.prettify())
+                
                 title = i.find('a', class_="item-link")['title']
-                link = 'https://www.idealista.com'+i.find('a', class_="item-link")['href']
+                link = 'https://www.idealista.com' + i.find('a', class_="item-link")['href']
                 features = [span.get_text() for div in i.find_all('div', class_='item-detail-char') for span in div.find_all('span')]
-                price_row = i.find('div',class_='price-row')
-                price_sim = price_row.find('span', class_='item-price h2-simulated').get_text() if price_row.find('span', class_='item-price h2-simulated') else None
-                price_down = price_row.find('span', class_='pricedown_price').get_text() if price_row.find('span', class_='pricedown_price') else None
+                price_row = i.find('div', class_='price-row')
+                price_sim = safe_get_text(price_row, 'item-price h2-simulated')
+                price_down = safe_get_text(price_row, 'pricedown_price')
                 tags = [span.get_text() for div in i.find_all('div', class_='listing-tags-container') for span in div.find_all('span')]
-                description = i.find('div',class_='item-description').get_text() if i.find('div',class_='item-description') else None 
+                description = i.find('div', class_='item-description').get_text() if i.find('div', class_='item-description') else None
                 rooms = features[0] if len(features) > 0 else None
                 size = features[1] if len(features) > 1 else None
                 floor = features[2] if len(features) > 2 else None
+
                 print(title)
-                print('\n')
-                n=n+1    
-        
+                n += 1
+
                 # Append property info to list of properties
                 properties.append({
-                   'Title':title
-                   ,'Description':description
-                   ,'Price_sim':price_sim
-                   ,'Price_down':price_down 
-                   ,'Features':features
-                   ,'Tags':tags
-                   ,'Rooms': rooms
-                   ,'Size': size
-                   ,'Floor': floor
-                   ,'Link':link
+                   'Title': title,
+                   'Description': description,
+                   'Price_sim': price_sim,
+                   'Price_down': price_down,
+                   'Features': features,
+                   'Tags': tags,
+                   'Rooms': rooms,
+                   'Size': size,
+                   'Floor': floor,
+                   'Link': link
                 })
-  
+
         except Exception as e:
             print(f"An error occurred on page {page}: {e}")
 
 finally:
-    driver.quit()  # Ensure the driver is closed after all iterations
-
+    driver.quit()  # Ensure the driver is closed
 
 # Convert to DataFrame
 df = pd.DataFrame(properties)
-print("DataFrame created")
-print("\nDescription")
-print(df.describe())
-print("\nInfo")
-print(df.info())
-print("\nHead")
-print(df.head)
 
-# Export to Excel
-path = 'C:/Users/Julian/Downloads'
-filename = 'idealista_eixample.xlsx'
-df.to_excel(f'{path}/{filename}', index=False)
-
-print(f"Data exported to {path}/{filename}")
+if not df.empty:
+    # Export to Excel
+    path = 'C:/Users/Julian/Downloads'
+    filename = 'idealista_eixample.xlsx'
+    df.to_excel(f'{path}/{filename}', index=False)
+    print(f"Data exported to {path}/{filename}")
+else:
+    print("No data to export.")
